09f241c775eec3e6671ae135abe8950212073b75
Jiaming Hu <Jiaming.Hu@ibm.com>
2020-06-10 15:34:47 -0400

    Upgrade operator-sdk version

--- pkg/controller/podpreset/podpreset_controller.go
+++ pkg/controller/podpreset/podpreset_controller.go
@@ -17,14 +17,9 @@
 package podpreset
 
 import (
-	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
-	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
-	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/builder"
 )
 
 const (
@@ -40,57 +35,12 @@ var log = logf.Log.WithName("controller_podpreset")
 
 func Add(mgr manager.Manager) error {
 
-	ns, err := k8sutil.GetWatchNamespace()
+	// Setup webhooks
+	log.Info("setting up webhook server")
+	hookServer := mgr.GetWebhookServer()
 
-	if err != nil {
-		return err
-	}
-
-	mutatingWebhook, err := builder.NewWebhookBuilder().
-		Name(podpresetName).
-		Mutating().
-		Operations(admissionregistrationv1beta1.Create).
-		WithManager(mgr).
-		ForType(&corev1.Pod{}).
-		FailurePolicy(admissionregistrationv1beta1.Ignore).
-		Handlers(&Mutator{}).
-		Build()
-
-	if err != nil {
-		log.Error(err, "Error occurred building mutating webhook")
-		return err
-	}
-
-	svr, err := webhook.NewServer(webhookName, mgr, webhook.ServerOptions{
-		Port:    serverPort,
-		CertDir: certDir,
-		BootstrapOptions: &webhook.BootstrapOptions{
-			Secret: &types.NamespacedName{
-				Namespace: ns,
-				Name:      webhookSecretName,
-			},
-			Service: &webhook.Service{
-				Namespace: ns,
-				Name:      webhookName,
-				Selectors: map[string]string{
-					"name": webhookName,
-				},
-			},
-			MutatingWebhookConfigName: webhookConfigName,
-		},
-	})
-
-	if err != nil {
-		log.Error(err, "Error occurred creating webhook server")
-		return err
-	}
-
-	err = svr.Register(mutatingWebhook)
-
-	if err != nil {
-		log.Error(err, "Error occurred registrying webhook server")
-		return err
-	}
+	log.Info("registering webhooks to the webhook server")
+	hookServer.Register("/mutate-ibm-cs-pod", &webhook.Admission{Handler: &Mutator{}})
 
 	return nil
 }
